2b55378ec724eec7a3fcbd45a7974f5a4aba715e
i7akem <m7akem@me.com>
2015-06-20 23:51:05 +0800

    Initial Commit

---
+++ Sign In FormTests/Sign_In_FormTests.swift
@@ -0,0 +1,36 @@
+//
+//  Sign_In_FormTests.swift
+//  Sign In FormTests
+//
+//  Created by i7akem on 6/20/15.
+//  Copyright (c) 2015 i7akem. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Sign_In_FormTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
